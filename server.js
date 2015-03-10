75bbb06fd80dd3a44323870ff5b2ee760b177939
Pawel <pawel745@gmail.com>
2015-03-10 14:39:10 -0400

    ...

---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require('express')
+var app = express()
+
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
+
+
+app.get('/', function(req,res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+var myState = "";
+
+app.get('/submit', function (req, res) {
+    myState = req.query.q;
+    res.send(myState);
+})
+
+app.get(/^(.+)$/, function(req, res) { //file server for the rest of the files in /public
+                                       //see comments at http://stackoverflow.com/questions/9443840/basic-webserver-with-node-js-and-express-for-serving-html-file-and-assets
+    try{
+        res.sendFile(__dirname +  '/public/' + req.params[0]);
+    }catch(err){
+        console.log("file err ", err);
+    }
+
+});
+
+
+
+var server = app.listen(3002, function () {
+
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('server listening at http://%s:%s', host, port)
+
+})
